275cec72f1d5dc5f9577d9973a178338f7117f02
nikrenxd <nikrenc1101@gmail.com>
2024-10-15 15:56:03 +0300

    tests for TagViewSet, and base class for tests

--- tests/notes/test_notes.py
+++ tests/notes/test_notes.py
@@ -1,26 +1,18 @@
 import pytest
 from django.contrib.auth import get_user_model
 from rest_framework import status
-from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
 
+from tests.utils import TestsBase
 
 User = get_user_model()
 
 
 @pytest.mark.django_db
-class TestNotesViewSetBase:
-    base_url = reverse("notes-list")
+class TestNotesViewSet(TestsBase):
+    base_url_name = "notes-list"
+    detail_url_name = "notes-detail"
 
-    @staticmethod
-    def _dynamic_url(note_id: int) -> str:
-        return reverse("notes-detail", kwargs={"pk": note_id})
-
-    def action_url(self, note_id: int) -> str:
-        return self._dynamic_url(note_id) + "tags/"
-
-
-class TestNotesViewSetRoutes(TestNotesViewSetBase):
     def test_notes_create(self, authenticated_client: APIClient, user: User):
         note_data = {
             "title": "Test Note",
@@ -40,10 +32,16 @@ class TestNotesViewSetRoutes(TestNotesViewSetBase):
         assert response.status_code == status.HTTP_200_OK
         assert len(response.data) != 0
 
+    def test_notes_list_filter(self, authenticated_client: APIClient):
+        response = authenticated_client.get(
+            self.base_url,
+            query_params={"tag_name": "tag1"},
+        )
 
-class TestNotesViewSetIdRoutes(TestNotesViewSetBase):
+        assert response.data[0].get("tags")[0].get("name") == "tag1"
+
     def test_notes_detail(self, authenticated_client: APIClient):
-        response = authenticated_client.get(self._dynamic_url(1))
+        response = authenticated_client.get(self.detail_url(1))
 
         assert response.status_code == status.HTTP_200_OK
         assert response.data.get("id") == 1
@@ -51,29 +49,39 @@ class TestNotesViewSetIdRoutes(TestNotesViewSetBase):
 
     def test_notes_update(self, authenticated_client: APIClient):
         new_data = {"title": "Updated title", "content": "Updated content"}
-        response = authenticated_client.put(self._dynamic_url(1), data=new_data)
+        response = authenticated_client.put(self.detail_url(1), data=new_data)
 
         assert response.status_code == status.HTTP_200_OK
         assert response.data.get("id") == 1
         assert response.data.get("title") == "Updated title"
         assert response.data.get("content") == "Updated content"
 
+    def test_notes_partial_update(self, authenticated_client: APIClient):
+        new_data = {"content": "Updated content"}
+        response = authenticated_client.patch(self.detail_url(1), data=new_data)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data.get("content") == "Updated content"
+
     def test_notes_delete(self, authenticated_client):
-        response = authenticated_client.delete(self._dynamic_url(1))
+        response = authenticated_client.delete(self.detail_url(1))
 
         assert response.status_code == status.HTTP_204_NO_CONTENT
 
-
-class TestNotesViewSetActions(TestNotesViewSetBase):
     def test_notes_add_tags(self, authenticated_client: APIClient):
         tag_name = {"name": "tag3"}
-        response = authenticated_client.post(self.action_url(2), data=tag_name)
+        response = authenticated_client.post(
+            self.action_url(2, "tags"),
+            data=tag_name,
+        )
 
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data.get("name") == "tag3"
 
     def test_notes_remove_tags(self, authenticated_client: APIClient):
-        query_param = "?tag_id=2"
-        response = authenticated_client.delete(self.action_url(2) + query_param)
+        response = authenticated_client.delete(
+            self.action_url(2, "tags"),
+            query_params={"tag_id": 2},
+        )
 
         assert response.status_code == status.HTTP_204_NO_CONTENT
